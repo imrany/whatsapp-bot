e857bf73017d03cb501df4b96ba7b8f7462fab79
imranyy <imranmatano6@gmail.com>
2022-10-03 15:05:57 +0300

    third tut

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const qrcode = require('qrcode-terminal');
 
-const { Client } = require('whatsapp-web.js');
-const client = new Client();
+const { Client, LocalAuth } = require('whatsapp-web.js');
+const client = new Client({
+    authStrategy: new LocalAuth()
+});
 
 client.on('qr', qr => {
     qrcode.generate(qr, {small: true});
@@ -9,6 +11,10 @@ client.on('qr', qr => {
 
 client.on('ready', () => {
     console.log('Client is ready!');
+});
+
+client.on('message', message => {
+	console.log(message.body);
 });
 
 client.initialize();
