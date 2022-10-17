3cc9ad74bc40381e1700f0c88e1f6fc127aec71c
imranyy <imranmatano6@gmail.com>
2022-10-17 11:06:48 +0300

    first commit on branch2

---
+++ whatsapp-web.js
@@ -0,0 +1,75 @@
+const { default: axios } = require('axios');
+const qrcode = require('qrcode-terminal');
+const { Client, NoAuth, MessageMedia } = require('whatsapp-web.js');
+const $server=new Client({
+        puppeteer:{
+            headless: true ,
+            args: [
+                '--no-sandbox',
+                '--disable-setuid-sandbox'
+            ]
+        },
+        authStrategy: new NoAuth()
+    });
+    $server.on('qr', qr => {
+       qrcode.generate(qr, {small: true});
+       app.get('/',async(req,res)=>{
+        try{
+            res.json(`${qr}`);
+        } catch(err){
+            console.log(err.message);
+        }
+    })
+    });
+    
+    $server.on('ready', () => {
+        console.log('Client is ready!');
+    });
+    
+    $server.initialize();
+    $server.on('message',async message => {
+        const content=message.body;
+        if(content === '.commands') {
+            message.reply(`
+            (*List of commands to use*)
+            >.hello - calls out the bot.
+            >.joke - generate random jokes.
+            >.meme - generates random memes.
+            >.goodnight - sleeps the bot.
+            `);
+        }
+        if(content === '.hello') {
+            message.reply(`
+            Hello, Imran is not available.
+            you are talking to his chatbot.
+            my name is *Antonne*💀..
+            To proceed type *.commands*
+            `);
+        }
+        if(content === '.goodnight') {
+            message.reply(`
+            Who the fuck do you think you are?
+            i go sleep when i want to!!
+            *LOSER🤣🤣*
+            `);
+        }
+        if(content === '.meme') {
+           try {
+            message.react('😁');
+            const meme=await axios("https://meme-api.herokuapp.com/gimme").then(res=>res.data);
+            message.reply(await MessageMedia.fromUrl(meme.url));
+           } catch (error) {
+            message.reply("I'm out of memes");
+           }
+        } else if(content === '.joke') {
+            try {
+                message.react('😩');
+                const joke=await axios("https://v2.jokeapi.dev/joke/Any?safe-mode").then(res=>res.data);
+                const jokeMsg=await $server.sendMessage(message.from,joke.setup||joke.joke);
+                if(joke.delivery) setTimeout(()=>{jokeMsg.reply(joke.delivery)},5000);
+            } catch (error) {
+                $server.sendMessage(message.from,'Try next time🤣');
+            }
+        }
+    });
+    module.exports=$server;
